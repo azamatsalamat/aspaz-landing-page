870eee6d0119894965de54d4997d15cfaf577698
Azamat Salamat <azamat.salamat@samgau.com>
2024-08-31 20:08:44 +0500

    add section-title shared component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { BannerComponent } from "./banner/banner.component";
+import { SectionTitleComponent } from "./shared/section-title/section-title.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, BannerComponent],
+  imports: [RouterOutlet, HeaderComponent, BannerComponent, SectionTitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
