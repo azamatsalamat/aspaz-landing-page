17f2ad13e391ba6ff0edc598662c810437d1bb9f
Azamat Salamat <azamat.salamat@samgau.com>
2024-09-01 21:09:35 +0500

    add "why us" section

---
+++ src/app/orders-counter/orders-counter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './orders-counter.component.html',
+  styleUrl: './orders-counter.component.css'
+})
+export class OrdersCounterComponent {
+
+}
